7225f9b28d8f876dd8a3446cd09991d4166944cf
DarrenGebler <darreng968@gmail.com>
2019-05-16 11:48:56 +1000

    Drawing Ellipses on screen
    paintingTrail painting ellipses on screen

--- src/Painting.java
+++ src/Painting.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Painting extends JPanel {
+public class Painting extends JPanel{
     int x1 = 0;
     int x2 = 0;
     int y1 = 0;
     int y2 = 0;
 
-    public void paintComponent(Graphics g, int x1, int y1, int x2, int y2) {
+    public void paintComponent(Graphics g) {
         super.paintComponents(g);
+        g.setColor(Color.RED);
 
         g.drawRect(x1,x2,x2-x1,y2-y1);
 
@@ -24,6 +25,8 @@ public class Painting extends JPanel {
 
         Painting p = new Painting();
 
+        jf.setPreferredSize(new Dimension(640,480));
+        jf.setVisible(true);
         jf.add(p);
 
         return jf;
